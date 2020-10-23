a118bc64ace0703819b959ad6495bd7a3ff20ee4
atp <tarradas.adria@gmail.com>
2020-10-23 13:34:08 +0200

    Added Firebase

---
+++ javascripts/firebase_connection.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyD0GVyzFOYA7GuVebnWLhB9bhiq2KoIiCM",
+	authDomain: "website-github.firebaseapp.com",
+	databaseURL: "https://website-github.firebaseio.com",
+	projectId: "website-github",
+	storageBucket: "website-github.appspot.com",
+	messagingSenderId: "486288287704",
+	appId: "1:486288287704:web:b61895ff6e22ce641a34ad",
+	measurementId: "G-X9B6YG7YXH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
